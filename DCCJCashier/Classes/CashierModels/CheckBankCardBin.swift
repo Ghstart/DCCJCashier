42281e0b1acadc0f2d0fe99639b4bdfb158dc588
龚欢 <gonghuan2020@gmail.com>
2018-08-27 20:54:21 +0800

    add card type enum

--- DCCJCashier/Classes/CashierModels/CheckBankCardBin.swift
+++ DCCJCashier/Classes/CashierModels/CheckBankCardBin.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 
-public struct CheckBankCardBinResponse: Codable {
-    public var resultCode: String
-    public var data: CheckBankCardBindDetailResponse?
-    public var resultMessage: String
-    public var success: Bool
+public enum CheckBankCardBinTypes: Int, Codable {
+    case debit = 1
+    case credit
+    case cunzhe
+    case unknow
+    
+    var displayName: String {
+        switch self {
+        case .debit:
+            return "储蓄卡"
+        case .credit:
+            return "信用卡"
+        case .cunzhe:
+            return "存折"
+        case .unknow:
+            return "未知"
+        }
+    }
 }
 
 public struct CheckBankCardBindDetailResponse: Codable {
@@ -24,6 +37,6 @@ public struct CheckBankCardBindDetailResponse: Codable {
     public var cardAttribute: String
     public var cardStatus: String?
     public var cardStatusDesc: String?
-    public var cardType: Int
+    public var cardType: CheckBankCardBinTypes
     public var userReservedPhoneNumber: String?
 }
